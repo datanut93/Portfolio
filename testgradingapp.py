c5aacc7af25d5215a4b1514cc316fce149e0245f
datanut93 <64046589+datanut93@users.noreply.github.com>
2023-06-21 08:01:00 -0500

    Update testgradingapp.py

--- testgradingapp.py
+++ testgradingapp.py
@@ -1,53 +1,48 @@
+
 import streamlit as st
-import openai
+import requests
 
-# Set up your OpenAI API credentials
-openai.api_key = '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'
+OPENAI_API_KEY = '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'
+OPENAI_API_URL = 'https://api.openai.com/v1/engines/davinci-codex/completions'
 
-# Define your Streamlit app layout
 def main():
     st.title("Essay Grader")
-    
-    # User input: Prompt
+
     prompt = st.text_area("Enter the prompt:")
-    
-    # User input: Example answer
     example_answer = st.text_area("Enter the example answer:")
     
-    # Analyze and provide feedback
     if st.button("Analyze"):
         feedback = analyze_answer(prompt, example_answer)
         st.write(feedback)
-        
-# Function to analyze the answer and provide feedback
+
 def analyze_answer(prompt, example_answer):
-    # Generate response using Chat GPT
-    response = openai.Completion.create(
-        engine='text-davinci-003',
-        prompt=prompt + "\n\nExample Answer: " + example_answer,
-        max_tokens=100,
-        temperature=0.7,
-        n=1,
-        stop=None,
-        frequency_penalty=0.0,
-        presence_penalty=0.0
-    )
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {OPENAI_API_KEY}'
+    }
+
+    data = {
+        'prompt': f'{prompt}\n\nExample Answer: {example_answer}',
+        'max_tokens': 100,
+        'temperature': 0.7,
+        'n': 1,
+        'stop': None,
+        'frequency_penalty': 0.0,
+        'presence_penalty': 0.0
+    }
+
+    response = requests.post(OPENAI_API_URL, headers=headers, json=data)
+    response_data = response.json()
     
-    # Get the generated feedback
-    generated_feedback = response.choices[0].text.strip()
+    generated_feedback = response_data['choices'][0]['text'].strip()
+    grade = perform_grading(generated_feedback)
     
-    # Perform grading and provide feedback
-    grade = perform_grading(generated_feedback)  # Implement your grading logic here
     feedback = f"Generated Feedback: {generated_feedback}\nGrade: {grade}"
-    
     return feedback
 
-# Function to perform grading (replace with your grading logic)
 def perform_grading(generated_feedback):
     # Implement your grading logic here
-    # Assign a grade based on the analysis of the generated feedback
     grade = 'A'  # Replace with your grading logic
-    
     return grade
 
 if __name__ == '__main__':
