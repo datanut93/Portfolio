acd2be28fa0d8f4f7edb6981272c4525a6c4e15f
datanut93 <64046589+datanut93@users.noreply.github.com>
2023-06-21 19:16:09 -0500

    Update testgradingapp.py

--- testgradingapp.py
+++ testgradingapp.py
@@ -22,7 +22,7 @@ def analyze_answer(prompt, example_answer):
     }
 
     data = {
-        'prompt': f'{prompt}\n\nExample Answer: {example_answer}\n\nYou are acting as a tough high school english teacher grading essays. The example_answer provided is the student's essay. First compare your own perfect score response to the example answer to help determine what you grade the student's example answer. Ensuring their adherance to the prompt and answering all parts of the question should be critical in determing the example answer's grade on a scale of 1-100:',
+        'prompt': f'{prompt}\n\nExample Answer: {example_answer}\n\nYou are acting as a tough high school english teacher grading essays. The example_answer provided is the student essay. First compare what your high scoring response would be to the example answer to help determine what you grade the students example answer. Ensuring their adherance to the prompt and answering all parts of the question should be critical in determing the example answers grade on a scale of 1-100:',
         'max_tokens': 1000,
         'temperature': 0.7,
         'n': 1,
